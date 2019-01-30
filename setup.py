82d82a510b13b48b3117288d03174ae7f9cd6e24
Stephan Steinbach <steinbach@pixar.com>
2019-01-30 12:59:25 -0800

    checkpoint

--- setup.py
+++ setup.py
@@ -19,98 +19,15 @@ from skbuild import (
 )
 import setuptools
 
-# class CMakeExtension(Extension):
-#     def __init__(self, name, sourcedir=''):
-#         Extension.__init__(self, name, sources=[])
-#         self.sourcedir = os.path.abspath(sourcedir)
-#
-# class CMakeBuild(build_ext):
-#     def run(self):
-#         try:
-#             out = subprocess.check_output(['cmake', '--version'])
-#         except OSError:
-#             raise RuntimeError(
-#                 "CMake must be installed to build the following extensions: " +
-#                 ", ".join(e.name for e in self.extensions))
-#
-#         if platform.system() == "Windows":
-#             cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
-#                                          out.decode()).group(1))
-#             if cmake_version < '3.1.0':
-#                 raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-#
-#         for ext in self.extensions:
-#             self.build_extension(ext)
-#
-#     def build_extension(self, ext):
-#         extdir = os.path.abspath(
-#             os.path.dirname(self.get_ext_fullpath(ext.name)))
-#         # __import__('ipdb').set_trace()
-#         cmake_args = [
-#             '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-#             '-DPYTHON_EXECUTABLE=' + sys.executable,
-#             '-DCMAKE_INSTALL_PREFIX=' + extdir,
-#         ]
-#
-#         cfg = 'Debug' if self.debug else 'Release'
-#         build_args = ['--config', cfg]
-#
-#         if platform.system() == "Windows":
-#             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
-#                 cfg.upper(),
-#                 extdir)]
-#             if sys.maxsize > 2**32:
-#                 cmake_args += ['-A', 'x64']
-#             build_args += ['--', '/m']
-#         else:
-#             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-#             build_args += ['--', '-j10']
-#
-#         env = os.environ.copy()
-#         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-#             env.get('CXXFLAGS', ''),
-#             self.distribution.get_version())
-#         if not os.path.exists(self.build_temp):
-#             os.makedirs(self.build_temp)
-#         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
-#                               cwd=self.build_temp, env=env)
-#         subprocess.check_call(['cmake', '--build', '.'] + build_args,
-#                               cwd=self.build_temp)
-#         subprocess.check_call(['make', 'install'],
-#                               cwd=self.build_temp)
-#         # Copy *_test file to tests directory
-#         # test_bin = os.path.join(self.build_temp, 'tests')
-#         # self.copy_test_file('tests')
-#         print()  # Add an empty line for cleaner output
-#
-#     def copy_test_file(self, src_file):
-#         '''
-#         Copy ``src_file`` to ``dest_file`` ensuring parent directory exists.
-#         By default, message like `creating directory /path/to/package` and
-#         `copying directory /src/path/to/package -> path/to/package` are displayed on standard output. Adapted from scikit-build.
-#         '''
-#         # Create directory if needed
-#         dest_dir = os.path.join(os.path.dirname(
-#             os.path.abspath(__file__)), 'tests', 'bin')
-#         if dest_dir != "" and not os.path.exists(dest_dir):
-#             print("creating directory {}".format(dest_dir))
-#             os.makedirs(dest_dir)
-#
-#         # Copy file
-#         dest_file = os.path.join(dest_dir, os.path.basename(src_file))
-#         print("copying {} -> {}".format(src_file, dest_file))
-#         copyfile(src_file, dest_file)
-#         copymode(src_file, dest_file)
-#
-# def test_otio():
-#     """Discovers and runs tests"""
-#     try:
-#         # Clear the environment of a preset media linker
-#         del os.environ['OTIO_DEFAULT_MEDIA_LINKER']
-#     except KeyError:
-#         pass
-#     return unittest.TestLoader().discover('tests')
-#
+def test_otio():
+    """Discovers and runs tests"""
+    try:
+        # Clear the environment of a preset media linker
+        del os.environ['OTIO_DEFAULT_MEDIA_LINKER']
+    except KeyError:
+        pass
+    return unittest.TestLoader().discover('tests')
+
 setup(
     name='opentimelineio',
     version='0.9.1',
@@ -119,12 +36,36 @@ setup(
     description='Blah',
     long_description='',
     test_suite='setup.test_otio',
-    include_package_data=True,
+    include_package_data=False,
     packages=[
-        'py-opentimelineio'
-        # ,'opentimelineview' #todo
+        'opentimelineio',
+        'opentimelineview',
+        'opentimelineio_contrib',
     ],
-    package_dir={"":"src"},
+    package_dir={
+        "opentimelineview":"python/opentimelineview",
+        "opentimelineio":"python/py-opentimelineio",
+        "opentimelineio_contrib":"contrib/py-opentimelineio_contrib",
+    },
+    package_data={
+        'opentimelineio': [
+            'adapters/builtin_adapters.plugin_manifest.json',
+        ],
+        'opentimelineio_contrib': [
+            'adapters/contrib_adapters.plugin_manifest.json',
+        ]
+    },
     # because we need to open() the adapters manifest, we aren't zip-safe
     zip_safe=False,
+    tests_require=[
+            'mock;python_version<"3.3"',
+    ],
+    entry_points={
+        'console_scripts': [
+            'otioview = opentimelineview.console:main',
+            'otiocat = opentimelineio.console.otiocat:main',
+            'otioconvert = opentimelineio.console.otioconvert:main',
+            'otiostat = opentimelineio.console.otiostat:main',
+        ],
+    },
 )
